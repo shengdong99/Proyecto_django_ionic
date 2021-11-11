a4641e11504027a3b1259b8b841183cc00728da0
shengdong99 <shengdong100@gmail.com>
2021-11-11 20:07:42 +0000

    adsd

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Company, Products
+from .models import Company, Category, MenuElements, OtherElements
 
 admin.site.register(Company)
-admin.site.register(Products)
+admin.site.register(Category)
+admin.site.register(MenuElements)
+admin.site.register(OtherElements)
