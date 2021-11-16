136d2c1ae555a206775f048de51d0a622f49d9ac
shengdong99 <shengdong100@gmail.com>
2021-11-16 19:06:49 +0000

    dasd

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Company, Category, MenuElements, OtherElements
+from .models import Company, Category, CreatePage, MenuElements, OtherElements, Products
 
 admin.site.register(Company)
 admin.site.register(Category)
 admin.site.register(MenuElements)
 admin.site.register(OtherElements)
+admin.site.register(Products)
+admin.site.register(CreatePage)
--- backend/api/models.py
+++ backend/api/models.py
@@ -27,4 +27,20 @@ class OtherElements(models.Model):
     def __str__(self):
         return self.name
 
-    
+
+class Products(models.Model):
+    image = models.ImageField(null=False, blank=False)
+    title = models.CharField(max_length=200, null=False, blank=False)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+class CreatePage(models.Model):
+    title = models.CharField(max_length=200, null=False, blank=False)
+    image = models.ImageField(null=False, blank=False)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
